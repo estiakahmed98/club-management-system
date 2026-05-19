b9def124a0bf4626ab235a0692dc90fbd220d898
programmerrm <programmerwebrm@gmail.com>
2026-05-19 10:00:23 +0600

    dabatase seed

--- deploy.prod.sh
+++ deploy.prod.sh
@@ -4,8 +4,8 @@ set -e
 echo "Stopping existing containers..."
 docker compose -f docker-compose.prod.yml down
 
-echo "Starting Backend..."
-docker compose -f docker-compose.prod.yml up -d --build app
+echo "Starting services..."
+docker compose -f docker-compose.prod.yml up -d --build
 
 echo "Deployment completed successfully!"
 docker compose -f docker-compose.prod.yml ps
